1a1feae752b35ce600f8083c023e796614510989
Maxim Donchuk <m.donchuk@gmail.com>
2024-02-13 20:21:06 +0300

    add users urls to root urls file

---
+++ users/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from .views import MyTokenObtainPairView, UserViewSet, LoginAPIView
+from users.apps import UsersConfig
+
+app_name = UsersConfig.name
+
+router = DefaultRouter()
+router.register(r"users", UserViewSet, basename="users")
+
+urlpatterns = [
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('<int:pk>/detail/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', LoginAPIView.as_view(), name='login'),
+] + router.urls
