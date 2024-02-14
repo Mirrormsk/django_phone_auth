499758c807b685269c8cbbb2bab9d72e034af9e8
Maxim Donchuk <m.donchuk@gmail.com>
2024-02-14 10:09:02 +0300

    added verify url

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import MyTokenObtainPairView, UserViewSet, LoginAPIView
+from .views import MyTokenObtainPairView, UserViewSet, LoginAPIView, VerifyAPIView
 from users.apps import UsersConfig
 
 app_name = UsersConfig.name
@@ -13,4 +13,5 @@ urlpatterns = [
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('<int:pk>/detail/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/', LoginAPIView.as_view(), name='login'),
+    path('verify/', VerifyAPIView.as_view(), name='verify'),
 ] + router.urls
