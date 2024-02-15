bccf96e3f54a9f7b421377754779c05e9b0924a3
Maxim Donchuk <m.donchuk@gmail.com>
2024-02-15 20:32:43 +0300

    correct validator

--- users/validators.py
+++ users/validators.py
@@ -4,9 +4,6 @@ from rest_framework.exceptions import ValidationError
 def validate_phone(phone: str):
     if phone.startswith('+'):
         phone = phone[1:]
-    if phone.startswith('8'):
-        phone = f"7{phone[1:]}"
-
     if len(phone) < 11:
         raise ValidationError("Phone number length must be at least 11 digits")
     elif not phone.isdigit():
