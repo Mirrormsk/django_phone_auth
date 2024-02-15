7e0a3f22d4cce4fa2ac1f86ea11e4e1398b451ae
Maxim Donchuk <m.donchuk@gmail.com>
2024-02-15 20:32:13 +0300

    add SMSService abstract method and fake MySMSService class

--- users/services/sms.py
+++ users/services/sms.py
@@ -1,10 +1,27 @@
 import datetime
-
+from abc import ABC, abstractmethod
+import logging
 from django.conf import settings
 from django.utils import timezone
 from rest_framework.exceptions import ValidationError
 
 from users.models import User
+
+logger = logging.getLogger(__name__)
+
+
+class SMSService(ABC):
+    """Abstract base class for SMS service"""
+
+    @abstractmethod
+    def send_sms(self, to: str, message: str) -> None:
+        pass
+
+
+class MySMSService(SMSService):
+    """Fake SMS service"""
+    def send_sms(self, to: str, message: str) -> None:
+        print(f"Sending message to {to}: {message}")
 
 
 def time_since_code_sent(user: User, now: datetime.datetime) -> datetime.timedelta:
@@ -12,7 +29,12 @@ def time_since_code_sent(user: User, now: datetime.datetime) -> datetime.timedel
     return delta
 
 
-def send_verification_sms(user: User):
+def send_verification_sms(user: User, sms_service: SMSService) -> None:
+    """Sends verification sms to user"""
+
+    if not isinstance(sms_service, SMSService):
+        raise AttributeError("SMS-Service must be an instance of SMSService")
+
     now = timezone.localtime(timezone.now())
 
     if user.last_code_sent_time:
@@ -22,7 +44,13 @@ def send_verification_sms(user: User):
             time_remaining = int(timeout - time_delta.total_seconds())
             raise ValidationError(
                 {"message": f"You can get new code after {time_remaining} seconds", "error": "Timeout not expired"})
-
-    user.last_code_sent_time = now
-    user.save()
-    print(f"phone: {user.phone}, code: {user.otp_code}")
+    try:
+        sms_service.send_sms(
+            to=user.phone,
+            message=f"Your verification code is {user.otp_code}"
+        )
+    except Exception as e:
+        logger.exception(e)
+    finally:
+        user.last_code_sent_time = now
+        user.save()
