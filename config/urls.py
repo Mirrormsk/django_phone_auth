4aa97800a4c0cdcfe0affccffb81e5a258647b8e
Maxim Donchuk <m.donchuk@gmail.com>
2024-02-15 18:57:26 +0300

    added users urls

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("users/", include("users.urls", namespace="users"))
+    path("", include("users.urls", namespace="users"))
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import MyTokenObtainPairView, UserViewSet, LoginAPIView, VerifyAPIView
+from .views import MyTokenObtainPairView, UserViewSet, LoginAPIView, VerifyAPIView, MyTokenRefreshView, LoginView, \
+    UserDetailView
 from users.apps import UsersConfig
 
 app_name = UsersConfig.name
@@ -11,7 +12,9 @@ router.register(r"users", UserViewSet, basename="users")
 
 urlpatterns = [
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('<int:pk>/detail/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('login/', LoginAPIView.as_view(), name='login'),
+    path('token/refresh/', MyTokenRefreshView.as_view(), name='token_refresh'),
+    path('api/login/', LoginAPIView.as_view(), name='api_login'),
     path('verify/', VerifyAPIView.as_view(), name='verify'),
+    path("login/", LoginView.as_view(), name="login"),
+    path("detail/<int:pk>", UserDetailView.as_view(), name="detail")
 ] + router.urls
