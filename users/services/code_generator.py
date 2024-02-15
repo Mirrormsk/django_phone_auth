0e7aa6509e840cbbe284517dfed5e17107221200
Maxim Donchuk <m.donchuk@gmail.com>
2024-02-15 18:55:28 +0300

    code generator now have customisation

--- users/services/code_generator.py
+++ users/services/code_generator.py
@@ -2,10 +2,21 @@ import random
 import string
 
 
-def generate_random_code(length: int = 4) -> str:
+class CodeGenerator:
     """Generates random digits string
     :param length: length of the generated string
+    :param only_digits: if true code with only digits is generated
     :return: random digits string
     """
-    digits = string.digits
-    return ''.join(random.choice(digits) for _ in range(length))
+    def __init__(self, length: int = 4, only_digits: bool = True):
+        self.length = length
+        self.collection = string.digits
+
+        if not only_digits:
+            self.collection += string.ascii_letters
+
+    def generate_random_code(self) -> str:
+        return ''.join(random.choice(self.collection) for _ in range(self.length))
+
+    def __call__(self, *args, **kwargs):
+        return self.generate_random_code()
