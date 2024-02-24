21140ac008bec76ea817d59fcd9228af2cafe6fc
Maxim Donchuk <m.donchuk@gmail.com>
2024-02-24 09:45:55 +0300

    imports optimize

--- users/models.py
+++ users/models.py
@@ -3,8 +3,6 @@ import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from users.services.code_generator import CodeGenerator
-
 NULLABLE = {'blank': True, 'null': True}
 
 
